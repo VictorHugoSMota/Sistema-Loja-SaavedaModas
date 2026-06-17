a601b6871a060062a3e5003e9a81cd480f25d1f9
Victor Hugo <victorhugo.mota26@gmail.com>
2026-06-17 11:17:36 -0300

    Implementação de autenticação com BCrypt e Spring Security

---
+++ src/main/java/com/saavedramodas/loja/security/UsuarioDetailsService.java
@@ -0,0 +1,33 @@
+package com.saavedramodas.loja.security;
+
+import com.saavedramodas.loja.domain.entity.Usuario;
+import com.saavedramodas.loja.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(
+            String username)
+            throws UsernameNotFoundException {
+
+        Usuario usuario =
+                usuarioRepository
+                        .findByUsername(username)
+                        .orElseThrow(() ->
+                                new UsernameNotFoundException(
+                                        "Usuário não encontrado"
+                                ));
+
+        return User.builder()
+                .username(usuario.getUsername())
+                .password(usuario.getPassword())
+                .build();
+    }
+}
